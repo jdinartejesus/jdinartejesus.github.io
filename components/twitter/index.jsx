01da1dcedd30116f2121ffdd487d72b2ab34a914
Dinarte Jesus <jdinartejesus@gmail.com>
2016-12-12 17:22:22 +0100

    Add props to twitter component

--- components/twitter/index.jsx
+++ components/twitter/index.jsx
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
-export default class Twitter extends Component {
+class Twitter extends Component {
   render () {
+    const { tweet } = this.props
     return (
       <div className='twitter my4'>
         <div className='clearfix flex items-center'>
           <div className='col col-2 gray'>
-            <h5 className='gray m0'>Tweets</h5>
+            <h5 className='gray m0'>Tweet</h5>
+            <p className='h6 m0' style={{lineHeight: '.75rem'}}>{tweet.date}</p>
           </div>
           <div className='col col-10'>
-            <p className='tweets m0 left-align teal h5' style={{lineHeight: '1.2rem'}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor.</p>
+            <p className='tweets m0 left-align teal h5' style={{lineHeight: '1.2rem'}}>{tweet.text}</p>
           </div>
         </div>
       </div>
     )
   }
 }
+
+const { object } = PropTypes
+
+Twitter.propTypes = {
+  tweet: object
+}
+
+export default Twitter
