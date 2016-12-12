6bbc56f2940c0ae0602a185f43473e31492447ec
Dinarte Jesus <jdinartejesus@gmail.com>
2016-12-12 14:25:43 +0100

    Change 404 from markdown to reactjs

---
+++ pages/404.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './404.scss'
+
+class FourOrFour extends Component {
+  render () {
+    return (
+      <div className="four-or-four center mt4">
+        <img src="./images/404.svg" alt="404, page doesn't exit" height="109" width="120" />
+        <h1 className="four-or-four-title">NOT FOUND</h1>
+        <p className="four-or-four-sub">You just hit a route that doesn't exist... the sadness.</p>
+      </div>
+    )
+  }
+}
+
+export default FourOrFour
